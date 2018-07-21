e6c321ee27cc280600f46c0e14370bf21c3e4b7e
yingyang <393050600@qq.com>
2018-07-21 19:07:50 +0800

    包修改

--- hbase/src/main/java/com/hbase/pool/PoolLock.java
+++ hbase/src/main/java/com/hbase/pool/PoolLock.java
@@ -1,4 +1,4 @@
-package com.hbase.pool.hibernate;
+package com.hbase.pool;
 
 import java.util.concurrent.Semaphore;
 
@@ -7,11 +7,15 @@ import java.util.concurrent.Semaphore;
  */
 public class PoolLock {
     
-    public static final PoolLock
-            SUSPEND_RESUME_LOCK = new PoolLock();
+    public static final PoolLock SUSPEND_RESUME_LOCK = new PoolLock();
     
+    /**
+     * 并发的信号量中一共有 10000 个许可, 同一时间只有10000个线程能执行 acquire release 之间的代码,
+     * 但不能保证线程安全
+     */
     private static final int MAX_PERMITS = 10000;
     
+    // 使用该类是为了限制线程并发的数量
     private final Semaphore acquisitionSemaphore;
     
     /**
@@ -32,7 +36,11 @@ public class PoolLock {
     public void release() {
         acquisitionSemaphore.release();
     }
-    
+
+    /**
+     * 同一时间只有1个线程能执行acquire, release 之间的代码
+     * 因此是暂停
+     */
     public void suspend() {
         acquisitionSemaphore.acquireUninterruptibly(MAX_PERMITS);
     }
