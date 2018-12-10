2fbe49ff202d559a9b85e48b07c3a35c684ec3dd
yingyang <393050600@qq.com>
2018-12-10 17:35:23 +0800

    测试

--- script/run.sh
+++ script/run.sh
@@ -1,4 +1,4 @@
 fuser -k -n tcp 8080
 sleep 3
 cd /app/project/hbase-model/build/libs
-nohup java -jar hbase.war --logging.file=/app/project/hbase-model/log/hbase-model.log > /dev/null &
+nohup java -jar app.war --logging.file=/app/project/hbase-model/log/hbase-model.log > /dev/null &
