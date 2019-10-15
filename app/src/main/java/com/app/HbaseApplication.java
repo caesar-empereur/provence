160e061a28af8a9e8a75927a98ac92be108b2924
yingyang <393050600@qq.com>
2019-10-15 17:48:12 +0800

    文档修改

--- app/src/main/java/com/app/HbaseApplication.java
+++ app/src/main/java/com/app/HbaseApplication.java
@@ -1,15 +1,15 @@
 package com.app;
 
-import com.hbase.annotation.HbaseTableScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-//import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import com.hbase.annotation.HbaseTableScan;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author yingyang
@@ -18,13 +18,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @ComponentScan({ "com.app","com.hbase"})
 @EntityScan("com.app.model.jpa")
 @SpringBootApplication
-//@EnableMongoRepositories("com.app.repository.mongodb")
 @EnableSwagger2
 @ServletComponentScan
 @HbaseTableScan(modelPackage = "com.app.model.hbase", repositoryPackage = "com.app.repository.hbase")
 public class HbaseApplication implements WebMvcConfigurer {
-
-
 
     public static void main(String[] args) {
         SpringApplication.run(HbaseApplication.class);
